80cabc8a240212bc2a049479d5aa05eba549e2fa
leeosilvp <leo965679@gmail.com>
2025-08-31 21:38:58 -0300

    criando o footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import '../css/footer.css'
+
+function Footer(){
+    return(
+        <footer>
+            <div className="logo">
+                <a href="#"><img src="" alt="logo IMC" /></a>
+            </div>
+            <div className="social-links">
+                <a href="https://www.instagram.com/leeosilvp/" target='_blank'><i className='fa-brands fa-instagram'></i></a>
+                <a href="https://www.linkedin.com/in/leeosilvp/" target='_blank'><i className='fa-brands fa-linkedin-in'></i></a>
+                <a href="https://github.com/leoosilvp" target='_blank'><i className='fa-brands fa-github'></i></a>
+            </div>
+            <p>IMC &copy;2025 - Todos os direitos reservados. <a href="https://www.linkedin.com/in/leeosilvp/" target='_blank'>Leonardo Silva</a></p>
+        </footer>
+    )
+}
+
+export default Footer
